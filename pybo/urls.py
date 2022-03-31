4220a4d2cfa14abf31e092022e416f3a22d9f488
Mu-jun <suo24691@gmail.com>
2022-03-31 15:32:16 +0900

    Q&A html 제작 및 url연결

--- pybo/urls.py
+++ pybo/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'pybo' # 다른 앱에서 같은 url 별칭사용시 충돌방지
+
 urlpatterns = [
-    path('',views.index),
+    path('',views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('answer/create/<int:question_id>/',views.answer_create, name='answer_create'),
 ]
--- pybo/views.py
+++ pybo/views.py
@@ -1,6 +1,40 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+# from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Question,Answer
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse('안녕하세요 pybo에 오신것을 환영합니다.')
+    # return HttpResponse('안녕하세요 pybo에 오신것을 환영합니다.')
+    '''
+    pybo 목록 출력
+    '''
+    question_list = Question.objects.order_by('-create_date') # order_by 는 조회 결과를 정렬하는 함수
+    # -create_date : 작성일 역순으로
+    context = {'question_list': question_list}
+    return render(request, 'pybo/question_list.html',context)
+
+def detail(request, question_id):
+    '''
+    pybo 내용 출력
+    '''
+    # question = Question.objects.get(id=question_id) # id가 없을 때 500오류 보냄.
+    question = get_object_or_404(Question, pk=question_id) # id가 없을 때 404오류 보내기
+    context = {'question':question}
+    return render(request, 'pybo/question_detail.html',context)
+
+def answer_create(request, question_id):
+    '''
+    pybo 답변등록
+    '''
+    question = get_object_or_404(Question, pk=question_id)
+    # 방법1
+    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    # 방법2
+    '''
+    Answer모델을 사용하여 만들기
+    answer = Answer(question=question, content=request.POST.get('content'), create_date=timezone.now())
+    answer.save()
+    '''
+    return redirect('pybo:detail', question_id=question_id)
