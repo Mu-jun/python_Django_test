955a6257cd72959000d21e9b5605f523d1cce067
Mu-jun <suo24691@gmail.com>
2022-04-04 14:04:05 +0900

    '질문등록 기능추가'

--- pybo/views.py
+++ pybo/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Question,Answer
+from .forms import QuestionForm
 
 
 # Create your views here.
@@ -38,3 +39,18 @@ def answer_create(request, question_id):
     answer.save()
     '''
     return redirect('pybo:detail', question_id=question_id)
+
+def question_create(request):
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False) # 임시 저장(commit=False)하여 question 객체를 리턴받는다.
+            # QuestionForm 객체에는 현재 create_date 데이터가 없기 때문에 임시저장하여 값을 넣고나서 저장해야 한다.
+            question.create_date = timezone.now()
+            # create_date는 저장시점에 생성해야 하는 값이므로 저장할 때 생성하여 넣어준다.
+            question.save()
+            return redirect('pybo:index')        
+    else :
+        form = QuestionForm()
+        
+    return render(request, 'pybo/question_form.html', {'form':form})
