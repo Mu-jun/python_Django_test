9d8205b82fc236c9f7c67b5ebe6a5d4e890067c5
Mu-jun <suo24691@gmail.com>
2022-04-27 08:47:37 +0900

    static 경로변경 test

--- config/settings/production.py
+++ config/settings/production.py
@@ -3,6 +3,8 @@ from .base import *
 import os
 
 ALLOWED_HOSTS = ['13.209.74.184']
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = []
+STATIC_ROOT = []
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 DEBUG = False
