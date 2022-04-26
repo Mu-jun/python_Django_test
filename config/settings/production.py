e73980c4bcef28a083015361d3e7329b31859738
Mu-jun <suo24691@gmail.com>
2022-04-26 16:59:57 +0900

    static 경로 수정

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,7 +1,8 @@
 from tkinter import ALL
 from .base import *
+import os
 
 ALLOWED_HOSTS = ['13.209.74.184']
-STATIC_ROOT = BASE_DIR / 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = []
 DEBUG = False
