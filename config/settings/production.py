3bb0ad34f343228702182e2830104b8532223951
Mu-jun <suo24691@gmail.com>
2022-04-27 08:50:55 +0900

    static 경로 수정

--- config/settings/production.py
+++ config/settings/production.py
@@ -3,8 +3,8 @@ from .base import *
 import os
 
 ALLOWED_HOSTS = ['13.209.74.184']
-STATIC_ROOT = []
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
-DEBUG = False
+# STATIC_ROOT = []
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
+# DEBUG = False
