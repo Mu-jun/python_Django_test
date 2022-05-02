57b0e2a9bff86c0efa7be89b195c47d90b5959c2
Mu-jun <suo24691@gmail.com>
2022-05-02 12:35:11 +0900

    static 경로 변경

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,9 +6,7 @@ ALLOWED_HOSTS = ['13.209.74.184']
 
 # WSGI 서버(Gunicorn) 사용시
 
-STATIC_ROOT = [
-    os.path.join(BASE_DIR, 'static')    
-]
+STATIC_ROOT = [BASE_DIR/'static']
 STATICFILES_DIRS = []
 
 DEBUG = False # False면 static&media파일 제공안함
