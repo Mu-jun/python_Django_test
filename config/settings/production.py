000c5bcc8a0c6d65934d17b7d2f07adac6cd6001
Mu-jun <suo24691@gmail.com>
2022-05-04 08:55:43 +0900

    test

--- config/settings/production.py
+++ config/settings/production.py
@@ -5,9 +5,7 @@ ALLOWED_HOSTS = ['52.79.54.106', 'mujunkim.shop']
 
 # WSGI 서버(Gunicorn) 사용시
 
-STATIC_ROOT = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = []
 
 DEBUG = False # False면 static&media파일 제공안함
